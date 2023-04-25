5f71446305636363cdeb687ac6277a229b31e534
Alex Supryakov <sashaololoronny@gmail.com>
2023-04-25 13:12:25 +0300

    slider fixes

--- script/script.js
+++ script/script.js
@@ -1,47 +1,63 @@
-let init = false;
-let slider = document.querySelector('.brand-slider');
-let sliderWrapper = document.querySelector('.brand-slider__wrapper');
-let slide = document.querySelector('.brand-slider__slide');
-let showMore = document.querySelector('.brand-slider__show-more-button');
-let showMoreBox = document.querySelector('.brand-slider__show-more');
+let swiper = null;
+const mediaQuery768px = window.matchMedia("(max-width: 768px)");
+const mediaQuery1120px = window.matchMedia("(min-width: 1120px)");
+const paginationBox = document.querySelector(".swiper-pagination");
 
-showMore.addEventListener('click', function () {
+const slider = document.querySelector('.brand-slider');
+const sliderWrapper = slider.querySelector('.brand-slider__wrapper');
+const slide = sliderWrapper.querySelector('.brand-slider__slide');
+const showMoreBox = document.querySelector('.brand-slider__show-more');
+const showMore = showMoreBox.querySelector('.brand-slider__show-more-button');
+
+if (mediaQuery768px.matches) {
+   swiperInit();
+} else {
+   paginationBox.classList.toggle('_off');
+}
+
+function swiperInit() {
+   swiper = new Swiper(".swiper", {
+      spaceBetween: 16,
+      loop: true,
+      slidesPerView: 'auto',
+      pagination: {
+         el: ".swiper-pagination",
+         clickable: true,
+      },
+   });
+}
+
+function sliderToggle(isMobileSize) {
+   paginationBox.classList.toggle('_off');
+   if (isMobileSize && !swiper) {
+      swiperInit();
+   } else if (!isMobileSize && swiper) {
+      swiper.destroy();
+      swiper = null;
+   }
+}
+
+function sliderActions() {
    slider.classList.toggle('brand-slider-full');
    showMore.classList.toggle('_show');
    if (showMore.textContent === 'Скрыть') {
       showMore.textContent = 'Показать все';
    } else showMore.textContent = 'Скрыть';
-});
+}
 
 function newSlide() {
-   if (window.innerWidth >= 1120) {
+   if (mediaQuery1120px.matches) {
       let cloneSlide = slide.cloneNode(true);
       let newSlide = sliderWrapper.appendChild(cloneSlide);
       newSlide.classList.remove('swiper-slide');
    }
 }
 
-let newSlide1 = newSlide();
-let newSlide2 = newSlide();
+const newSlideAcer = newSlide();
+const newSlideApple = newSlide();
 
-function swiperCard() {
-   if (window.innerWidth <= 768) {
-      if (!init) {
-         init = true;
-         swiper = new Swiper(".brand-slider", {
-            spaceBetween: 16,
-            loop: true,
-            slidesPerView: 'auto',
-            pagination: {
-               el: ".swiper-pagination",
-               clickable: true,
-            },
-         });
-      }
-   } else if (init) {
-      swiper.destroy();
-      init = false;
-   }
-}
-swiperCard();
-window.addEventListener("resize", swiperCard);
+showMore.addEventListener('click', sliderActions);
+
+mediaQuery768px.addEventListener('change', function resizeWidth(event) {
+   sliderToggle(event.matches);
+})
